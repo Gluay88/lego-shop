fc3c965adc6891cedded67e42798a0628939d5c7
gluay88 <p.wantagaew@gmail.comm>
2022-09-17 22:43:17 -0600

    lego-gee

---
+++ README.md
@@ -0,0 +1,22 @@
+- npm create vite .
+- package name lego-shopping
+- react framework
+- react ts
+- npm i
+- npm run dev
+
+---
+
+- npm i react-router-dom bootstrap react-bootstrap
+- import bootstrap main.tsx
+- import "bootstrap/dist/css/bootstrap.min.css";
+
+---
+
+- folder structure
+- components and pages and data - json and stuff and context (react and redux)- hooks - utilities
+
+- router in main.tsx
+- import {Routes, Route} and {Container}
+
+-- create public folder
---
+++ src/pages/Store.tsx
@@ -0,0 +1,21 @@
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
+
+export function Store() {
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => {
+          return (
+            // <Col key={item.id}>{JSON.stringify(item)}</Col>
+            <Col key={item.id}>
+              <StoreItem {...item} />
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
+}
