a2668ab6554e6233eeda37b896ee02dbf094e5f8
Gluay <93889512+Gluay88@users.noreply.github.com>
2023-02-04 15:44:45 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/lego-shop/",
+  base: "/gluay88.github.io/lego-shop/",
   plugins: [react()],
 });
