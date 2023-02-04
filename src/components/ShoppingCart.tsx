94041ee16f1b4f9bd79ecd9bcb772094fd7d5bf0
gluay88 <p.wantagaew@gmail.comm>
2023-02-04 15:30:02 -0700

    hiya

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -34,7 +34,9 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
             )}
           </div>
           <div className="checkout">
-            <a href="/checkout">Proceed and Checkout</a>
+            <a href="https://gluay88.github.io/lego-shop/checkout">
+              Proceed and Checkout
+            </a>
           </div>
         </Stack>
       </Offcanvas.Body>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "/gluay88.github.io/lego-shop/",
+  plugins: [react()],
+});
